16887892ebe203e2a73ad8afce5952c3fcc05b80
Jose <jlcab8@gmail.com>
2022-05-14 19:28:51 +0200

    added mapping component Game on Homepage and basic styling

---
+++ src/components/Game.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Game = ({ name, releaseDate, image }) => {
+  return (
+    <StyledGame>
+      <h3>{name}</h3>
+      <p>{releaseDate}</p>
+      <img src={image} alt="" />
+    </StyledGame>
+  );
+};
+
+const StyledGame = styled(motion.div)`
+  text-align: center;
+  border-radius: 1rem;
+  box-shadow: 0 5px 30px rgba(0, 0, 0, 0.1);
+  img {
+    display: block;
+    width: 100%;
+    height: 309px;
+    object-fit: cover;
+  }
+`;
+
+export default Game;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,55 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesActions";
+// Components
+import Game from "../components/Game";
+// Styling & Animations
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, []); // OJO: SI NO PONEMOS EL ARRAY DEPENDENCY VACÍO, SE EJECUTA SIN PARAR. SI QUEREMOS QUE SE EJECUTE CADA VEZ QUE CAMBIE EL ESTADO DE ALGO SE PONE AHÍ DICHO ELEMENTO.
+
+  // Get the data back
+  // const games = useSelector((state) => state.games);
+  /* Extraemos lo que queremos de games  */
+  const { popular, upcoming, newGames } = useSelector((state) => state.games);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => {
+          return (
+            <Game
+              key={game.id}
+              name={game.name}
+              releaseDate={game.released}
+              image={game.background_image}
+            />
+          );
+        })}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0 5rem;
+  h2 {
+    padding: 5rem 0;
+  }
+`;
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
